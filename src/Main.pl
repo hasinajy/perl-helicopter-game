cec8ab479e48f4dbce6c37fa2ea37a0fea9e7e52
hasinajy <jyhasina@gmail.com>
2024-04-13 19:31:38 +0300

    feat: Add helicopter platforms

--- src/Main.pl
+++ src/Main.pl
@@ -9,5 +9,12 @@ $height = 450;
 my $mw = MainWindow->new;
 my $canvas = $mw->Canvas(-width => $width, -height => $height, -background => 'gray75')->pack;
 
+# ------------------------------- Terrain setup ------------------------------ #
+# Start platform
+$platform_1 = $canvas->create('rectangle', 0, 300, 100, 350, -fill => 'blue');
+
+# End platform
+$platform_2 = $canvas->create('rectangle', 700, 100, 800, 150, -fill => 'blue');
+
 # Run
 MainLoop;
