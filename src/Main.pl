4c1ee366568ee25191fecc40ad2b5287b17ed21d
hasinajy <jyhasina@gmail.com>
2024-04-13 19:30:14 +0300

    feat: Create basic window

--- src/Main.pl
+++ src/Main.pl
@@ -1,1 +1,13 @@
-print("Hello, World!");
+use Tk;
+
+# Space variables
+$block_size = 50;
+$width = 800;
+$height = 450;
+
+# Window setup
+my $mw = MainWindow->new;
+my $canvas = $mw->Canvas(-width => $width, -height => $height, -background => 'gray75')->pack;
+
+# Run
+MainLoop;
