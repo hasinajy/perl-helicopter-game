eceb45ffa0a5f261a6067f384ce0ed3e76e375b4
hasinajy <jyhasina@gmail.com>
2024-04-13 19:34:27 +0300

    style: Add section separator comments

--- src/Main.pl
+++ src/Main.pl
@@ -1,11 +1,11 @@
 use Tk;
 
-# Space variables
+# ------------------------------ Space variables ----------------------------- #
 $block_size = 50;
 $width = 800;
 $height = 450;
 
-# Window setup
+# ------------------------------- Window setup ------------------------------- #
 my $mw = MainWindow->new;
 my $canvas = $mw->Canvas(-width => $width, -height => $height, -background => 'gray75')->pack;
 
@@ -19,5 +19,5 @@ $start_pl = $canvas->create('rectangle', 0, 300, 100, 350, -fill => 'blue');
 # End platform
 $end_pl = $canvas->create('rectangle', 700, 100, 800, 150, -fill => 'blue');
 
-# Run
+# ------------------------------------ Run ----------------------------------- #
 MainLoop;
