8d323d2978ebf55b0fe21a338e1c102518dc5c93
hasinajy <jyhasina@gmail.com>
2024-04-16 22:28:50 +0300

    fix<sql>: Add auto-increment to shape_id in table<shape>

--- sql/shape.sql
+++ sql/shape.sql
@@ -3,11 +3,14 @@ CREATE DATABASE helicopter_game;
 Use helicopter_game;
 
 CREATE TABLE shape (
-    shape_id INT PRIMARY KEY,
+    shape_id INT PRIMARY KEY auto_increment,
     coord_string VARCHAR(256) NOT NULL,
     isPlatform INT DEFAULT 0
 );
 
 -- Shape
-INSERT INTO shape(coord_string) VALUES
-    ("[]-[]-[]-[]");
+INSERT INTO shape(coord_string, isPlatform) VALUES
+    ("[0;300]-[100;300]-[100;350]-[0;350]", -1),
+    ("[700;100]-[800;100]-[800;150]-[700;150]", 1),
+    ("[200;0]-[300;0]-[300;300]-[200;300]", 0),
+    ("[500;450]-[600;450]-[600;150]-[500;150]", 0);
