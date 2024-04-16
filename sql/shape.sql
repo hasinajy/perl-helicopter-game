8e4d76ea0fe9e6cb71552bb412bbde49c0eb259d
hasinajy <jyhasina@gmail.com>
2024-04-16 22:17:34 +0300

    refactor<sql>: Add column isPlatform to the table<shape> creation script

--- sql/shape.sql
+++ sql/shape.sql
@@ -4,5 +4,10 @@ Use helicopter_game;
 
 CREATE TABLE shape (
     shape_id INT PRIMARY KEY,
-    coord_string VARCHAR(256) NOT NULL
+    coord_string VARCHAR(256) NOT NULL,
+    isPlatform INT DEFAULT 0
 );
+
+-- Shape
+INSERT INTO shape(coord_string) VALUES
+    ("[]-[]-[]-[]");
