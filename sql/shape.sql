e803a2d223a60389f800253348da2c8099fcb74f
hasinajy <jyhasina@gmail.com>
2024-04-28 16:57:13 +0300

    sql: Add tank table and test data

--- sql/shape.sql
+++ sql/shape.sql
@@ -8,6 +8,12 @@ CREATE TABLE shape (
     isPlatform INT DEFAULT 0
 );
 
+CREATE TABLE tank (
+    tank_id INT PRIMARY KEY auto_increment,
+    coord_string VARCHAR(256) NOT NULL,
+    score INT
+);
+
 -- Shape
 INSERT INTO shape(coord_string, isPlatform) VALUES
     ("[0;300]-[100;300]-[100;350]-[0;350]", -1),
@@ -20,3 +26,10 @@ INSERT INTO shape(coord_string, isPlatform) VALUES
     ("[200;100]-[250;100]-[250;800]-[200;800]", 0),
     ("[350;70]-[400;70]-[400;800]-[350;800]", 0),
     ("[500;70]-[550;70]-[550;800]-[500;800]", 0);
+
+-- Tank
+INSERT INTO tank(coord_string, score) VALUES
+    ("[100;775]-[150;775]-[150;800]-[100;800]", 100),
+    ("[275;775]-[325;775]-[325;800]-[275;800]", 80),
+    ("[425;775]-[475;775]-[475;800]-[425;800]", 70),
+    ("[575;775]-[625;775]-[625;800]-[575;800]", 150);
